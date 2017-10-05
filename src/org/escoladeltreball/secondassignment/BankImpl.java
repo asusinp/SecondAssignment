1854f47ae8cea5cc9d1e14f4120ac1b6b5baa885
iaw21752927 <alexsusin1998@gmail.com>
2017-10-05 13:17:48 +0200

    BankImpl added and finished

---
+++ src/org/escoladeltreball/secondassignment/BankImpl.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package org.escoladeltreball.secondassignment;
+
+/**
+ * @author alex
+ *
+ */
+public class BankImpl implements Bank {
+	
+	protected String owner;
+	protected double balance;
+	
+	/**
+	 * @param owner the owner of this account
+	 * @param balance the actual balance of this account
+	 */
+	public BankImpl(String owner, double balance) {
+		this.owner = owner;
+		this.balance = balance;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.escoladeltreball.secondassignment.Bank#withdraw(double)
+	 */
+	@Override
+	public void withdraw(double euros) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.escoladeltreball.secondassignment.Bank#deposit(double)
+	 */
+	@Override
+	public void deposit(double euros) {
+		// TODO Auto-generated method stub
+		if (euros > 0) {
+			this.balance += euros;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.escoladeltreball.secondassignment.Bank#toDollars()
+	 */
+	@Override
+	public double toDollars() {
+		// TODO Auto-generated method stub
+		return balance * Bank.DOLAR_RATE;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.escoladeltreball.secondassignment.Bank#getBalance()
+	 */
+	@Override
+	public double getBalance() {
+		// TODO Auto-generated method stub
+		return this.balance;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.escoladeltreball.secondassignment.Bank#isBalanceNegative()
+	 */
+	@Override
+	public boolean isBalanceNegative() {
+		// TODO Auto-generated method stub
+		return this.balance < 0;
+	}
+
+}
