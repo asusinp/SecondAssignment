32ef01e6bf2078c65683140d405a9e4251f74e4a
iaw21752927 <alexsusin1998@gmail.com>
2017-10-05 20:09:39 +0200

    added test classes, cashAccountTest finished, creditAccountTest started

---
+++ test/org/escoladeltreball/secondassignment/cashAccountTest.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package org.escoladeltreball.secondassignment;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author iaw21752927
+ *
+ */
+public class cashAccountTest {
+	
+	private Bank bank;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		bank = new CashAccount("Alex", 2500);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		bank = null;
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.CashAccount#withdraw(double)}.
+	 */
+	@Test
+	public void testWithdraw() {
+		try {
+			bank.withdraw(250);
+			assertEquals(2250, bank.getBalance(), 1e-4);
+			bank.withdraw(-2555550);
+			assertEquals(2250, bank.getBalance(), 1e-4);	
+		}		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#deposit(double)}.
+	 */
+	@Test
+	public void testDeposit() {
+		bank.deposit(250);
+		assertEquals(2750, bank.getBalance(), 1e-4);
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#toDollars()}.
+	 */
+	@Test
+	public void testToDollars() {
+		assertEquals(3500,bank.toDollars(), 1e-4);
+		
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#getBalance()}.
+	 */
+	@Test
+	public void testGetBalance() {
+		assertEquals(2500, bank.getBalance(), 1e-4);
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#isBalanceNegative()}.
+	 */
+	@Test
+	public void testIsBalanceNegative() {
+		assertFalse(bank.isBalanceNegative());
+	}
+}
---
+++ test/org/escoladeltreball/secondassignment/creditAccountTest.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package org.escoladeltreball.secondassignment;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author iaw21752927
+ *
+ */
+public class creditAccountTest {
+	
+	private Bank bank;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		bank = new CreditAccount("Alex", 2500);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		bank = null;
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.CreditAccount#withdraw(double)}.
+	 */
+	@Test
+	public void testWithdraw() {
+		try {
+			bank.withdraw(250);
+			assertEquals(2250, bank.getBalance(), 1e-4);
+		}		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#deposit(double)}.
+	 */
+	@Test
+	public void testDeposit() {
+		
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#toDollars()}.
+	 */
+	@Test
+	public void testToDollars() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#getBalance()}.
+	 */
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#isBalanceNegative()}.
+	 */
+	@Test
+	public void testIsBalanceNegative() {
+		fail("Not yet implemented");
+	}
+
+}
